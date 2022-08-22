331cc68c38298e9c9f8477e0835540fcf33c3f63
George Lima <georgelima11@hotmail.com>
2022-08-22 14:34:10 -0300

    feat: add cast_and_apply!

--- lib/changesets.ex
+++ lib/changesets.ex
@@ -1,3 +1,7 @@
+defmodule ExPixUtils.ValidationError do
+  defexception [:message, :type, :changeset]
+end
+
 defmodule ExPixUtils.Changesets do
   import Ecto.Changeset
   alias Ecto.Changeset
@@ -10,6 +14,23 @@ defmodule ExPixUtils.Changesets do
     |> case do
       %{valid?: true} = changeset -> {:ok, apply_changes(changeset)}
       changeset -> {:error, {type, changeset}}
+    end
+  end
+
+  @spec cast_and_apply!(schema_module :: module(), params :: map(), type :: atom()) ::
+          struct()
+  def cast_and_apply!(schema_module, params, type \\ :validation) do
+    params
+    |> schema_module.changeset()
+    |> case do
+      %{valid?: true} = changeset ->
+        apply_changes(changeset)
+
+      changeset ->
+        raise ExPixUtils.ValidationError,
+          message: "Validation error",
+          type: type,
+          changeset: changeset
     end
   end
 end
