9596afa34d2d62ba4771c9adc036f3b42f9157a1
George Lima <georgelima11@hotmail.com>
2022-08-22 09:04:43 -0300

    feat: add Transfer model

---
+++ lib/models/transfer.ex
@@ -0,0 +1,29 @@
+defmodule ExPixUtils.Models.Transfer do
+  use ExPixUtils.Models.Base
+
+  @required [:id, :e2eId, :valor, :status]
+
+  embedded_schema do
+    field(:id, :string)
+    field(:e2eId, :string)
+    field(:valor, ExPixUtils.Ecto.Money)
+
+    embeds_one :horario, Calendar, primary_key: false do
+      field(:solicitacao, :utc_datetime)
+    end
+
+    field(:status, Ecto.Enum, values: ~w(EM_PROCESSAMENTO REALIZADO NAO_REALIZADO)a)
+  end
+
+  def changeset(model \\ %__MODULE__{}, params) do
+    model
+    |> cast(params, @required)
+    |> cast_embed(:horario, required: true, with: &calendar_changeset/2)
+    |> validate_required(@required)
+  end
+
+  defp calendar_changeset(model, params),
+    do:
+      model
+      |> cast(params, [:solicitacao])
+end
